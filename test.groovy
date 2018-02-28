3a947c0fae8c81e987c0421356b09c3fc4c6ff01
Christoph <cruepprich@gmail.com>
2018-02-28 15:46:49 -0600

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,8 +1,5 @@
-pipelineRepo = "ttps://github.com/cruepprich/pins.git"
-pipelineBranch = "master"
-script = "test.groovy"
-
-node () {
-    git poll: false, changelog: false, url: pipelineRepo, credentialsId: "git-credentials", branch: pipelineBranch
-    load script    
+node {
+  stage ("stage 1") {
+    echo "hello world"
+  }
 }
